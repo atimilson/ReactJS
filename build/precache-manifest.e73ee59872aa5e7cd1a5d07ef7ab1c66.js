9a8c4229ddb83ced32b1b03100faeb8bc527b41f
atimilson <atimilson95@gmail.com>
2020-07-28 20:16:30 -0400

    Build

---
+++ build/precache-manifest.e73ee59872aa5e7cd1a5d07ef7ab1c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03defd639ec30744bb157e2b06b336f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "280c40b22595b156a4cb",
+    "url": "/static/css/main.345ea303.chunk.css"
+  },
+  {
+    "revision": "a231447c889ffcbe7096",
+    "url": "/static/js/2.23d03751.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.23d03751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280c40b22595b156a4cb",
+    "url": "/static/js/main.ced39bf8.chunk.js"
+  },
+  {
+    "revision": "f02eb603396bec2df3d8",
+    "url": "/static/js/runtime-main.6224c0b6.js"
+  }
+]);
